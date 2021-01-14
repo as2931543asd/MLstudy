8ed7dcbcb99966312867f4f55fd24f649286fd89
涛 江 <szhx20208@outlook.com>
2021-01-14 18:26:26 +0800

    add encoder

---
+++ Seq2Seq Model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emd_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        self.embedding = nn.Embedding(input_dim, emd_dim)
+        self.rnn = nn.GRU(emd_dim, hid_dim, n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+        outputs, hidden = self.rnn(embedded)
+        return hidden
